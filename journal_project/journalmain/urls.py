6cefa8f4b5e06dc9ed55cfc782102b381869cc16
Christopher Fischer <chris@null.local>
2025-05-23 19:44:11 +0000

    tenative journal detail view

--- journal_project/journalmain/urls.py
+++ journal_project/journalmain/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('journals/', views.journals, name='journals'),
+    path('details/<int:id>/', views.journal_detail, name='journal_detail'),  
 ]
--- journal_project/journalmain/views.py
+++ journal_project/journalmain/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import JournalEntry
 
@@ -7,3 +7,7 @@ from .models import JournalEntry
 def journals(request):
     journal_entries = JournalEntry.objects.all()
     return render(request, 'journalindex.html', {'journal_entries': journal_entries}) #HttpResponse("Hello world!")
+
+def journal_detail(request, id):
+    entry = get_object_or_404(JournalEntry, id=id)
+    return render(request, 'journal_detail.html', {'entry': entry})
