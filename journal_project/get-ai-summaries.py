659a58a0a260c7f3166788b3e6e84c54965a3a94
Chris Fischer <test@test.local>
2025-05-26 03:51:06 -0400

    copy/paste error from nano resulted in my prompts getting truncated

--- journal_project/get-ai-summaries.py
+++ journal_project/get-ai-summaries.py
@@ -1,9 +1,8 @@
 import requests
 import os
 
-mood_prompt = """You're an AI who is unbiased and is tasked with analyzing a journal/diary entry below. You've been given the following mood list, in python3 list format, to use as possible outputs (important! you may ONLY output elements in >
-title_prompt = """You're an AI who is unbiased and is tasked with analyzing a journal/diary entry below. You've been given the following task: Generate a short (important! less then 100 characters) title that describes a main topic of the jou>
-
+mood_prompt = """You're an AI who is unbiased and is tasked with analyzing a journal/diary entry below. You've been given the following mood list, in python3 list format, to use as possible outputs (important! you may ONLY output elements in the list). You're tasked with reading the journal entry and pulling out moods from it. Your output should be a list of moods, no more then 7 moods. It can be as short as possible. If it's impossible to pull out any moods at all (this should be unlikely!), you will output an empty python list. In all cases, the outputted list should be a valid python list.\n\n```python3\nmoods = [\n    'happy',\n    'sad',\n    'angry',\n    'afraid',\n    'excited',\n    'calm',\n    'worried',\n    'in love',\n    'surprised',\n    'proud',\n    'ashamed',\n    'frustrated',\n    'guilty',\n    'curious',\n    'nostalgic',\n    'hopeful',\n    'disappointed',\n    'embarrassed',\n    'envious',\n    'grateful',\n    'longing',\n    'relieved',\n    'optimistic',\n    'pessimistic'\n]\n```\n\nJournal Entry: \n"""
+title_prompt = """You're an AI who is unbiased and is tasked with analyzing a journal/diary entry below. You've been given the following task: Generate a short (important! less then 100 characters) title that describes a main topic of the journal. The title should be something that reminds the author of what they wrote about if they saw a list of titles. Your output, after thinking, should just be the title with no formatting.\nJournal Entry: \n"""
 
 def process_entry(entry, tries):
     if tries > 5:
