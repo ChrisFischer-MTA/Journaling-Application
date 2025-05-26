b6f70ff91e9c9ece7668f688bbddbc49ce638c20
Chris Fischer <test@test.local>
2025-05-26 05:59:44 -0400

    added report to admin, set up the relationship between journals and reports

--- journal_project/journalmain/admin.py
+++ journal_project/journalmain/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import JournalEntry, Blurb
+from .models import JournalEntry, Blurb, Report
 
 # Register your models here.
 admin.site.register(JournalEntry)
 admin.site.register(Blurb)
+admin.site.register(Report)
