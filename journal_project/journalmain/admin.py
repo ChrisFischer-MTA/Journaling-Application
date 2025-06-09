cf6ca88d844ee6a9550b1449497ccb60f85f2487
Chris Fischer <christopherrfischer@outlook.com>
2025-06-09 15:23:46 -0400

    Adding Goal to Admin Site

--- journal_project/journalmain/admin.py
+++ journal_project/journalmain/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import JournalEntry, Blurb, Report
+from .models import JournalEntry, Blurb, Report, Goal
 
 # Register your models here.
 admin.site.register(JournalEntry)
 admin.site.register(Blurb)
 admin.site.register(Report)
+admin.site.register(Goal)
