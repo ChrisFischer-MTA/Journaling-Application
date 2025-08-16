6eeb96b335a29315eee105fa10e3d1ab28e5d4aa
Chris Fischer <christopherrfischer@outlook.com>
2025-08-16 06:17:03 -0400

    changing sqlite3 driver to postgres

--- journal_project/journal_project/settings.py
+++ journal_project/journal_project/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'journal_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("JOURNAL_APP_DB_NAME"),
+        'USER': os.environ.get("JOURNAL_APP_DB_USER_NAME"),
+        'PASSWORD': os.environ.get("JOURNAL_APP_DB_PASSWORD"),
+        'HOST': os.environ.get("JOURNAL_APP_DB_HOST"),
+        'PORT': os.environ.get("JOURNAL_APP_DB_PORT"),
     }
 }
 
